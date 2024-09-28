d4be704fbcd5607f8c127ea95b6091c690f56f51
Nabiur siddique <nabiursiddique01@gmail.com>
2024-09-29 02:12:53 +0600

    Loading state is handled better now

--- src/app/(WithCommonLayout)/login/page.tsx
+++ src/app/(WithCommonLayout)/login/page.tsx
@@ -18,12 +18,9 @@ const LogIn = () => {
     handleUserLogin(userData);
   };
 
-  if (isPending && !isSuccess) {
-    return <Loading />;
-  }
-
   return (
     <>
+      {isPending && !isSuccess && <Loading />}
       <div className="flex h-[calc(100vh-200px)] w-full flex-col items-center justify-center">
         <h3 className="my-2 text-3xl font-bold uppercase">Log In</h3>
         <p className="mb-4">Welcome Back! Let&lsquo;s Get Started</p>
--- src/app/(WithCommonLayout)/register/page.tsx
+++ src/app/(WithCommonLayout)/register/page.tsx
@@ -22,58 +22,61 @@ const Register = () => {
     handleUserRegistration(userData);
   };
 
-  if (isPending && !isSuccess) {
-    return <Loading />;
-  }
-
   return (
-    <div className="flex h-[calc(100vh-100px)] flex-col items-center justify-center">
-      <h3 className="my-2 text-3xl font-bold uppercase">Register</h3>
-      <p className="mb-4">Let's Get Started a new journey</p>
-      <div className="lg:w-[35%] w-[90%]">
-        <PHForm
-          onSubmit={onSubmit}
-          resolver={zodResolver(registerValidationSchema)}
-          //! For Development only
-          defaultValues={{
-            name: "Nabiur Siddique",
-            email: "nabiursiddique01@gmail.com",
-            password: "123456",
-            profileImage:
-              "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png",
-          }}
-        >
-          <div className="py-3">
-            <PHInput label="Name" name="name" size="sm" />
-          </div>
-          <div className="py-3">
-            <PHInput label="Email" name="email" size="sm" />
-          </div>
-          <div className="py-3">
-            <PHInput
-              label="Password"
-              name="password"
-              size="sm"
-              type="password"
-            />
-          </div>
-          <div className="py-3">
-            <PHInput label="Profile Image URL" name="profileImage" size="sm" />
-          </div>
-
-          <Button
-            className="my-3 w-full rounded-md bg-default-900 text-default"
-            size="lg"
-            type="submit"
+    <>
+      {isPending && !isSuccess && <Loading />}
+      <div className="flex h-[calc(100vh-100px)] flex-col items-center justify-center">
+        <h3 className="my-2 text-3xl font-bold uppercase">Register</h3>
+        <p className="mb-4">Let's Get Started a new journey</p>
+        <div className="lg:w-[35%] w-[90%]">
+          <PHForm
+            onSubmit={onSubmit}
+            resolver={zodResolver(registerValidationSchema)}
+            //! For Development only
+            defaultValues={{
+              name: "Nabiur Siddique",
+              email: "nabiursiddique01@gmail.com",
+              password: "123456",
+              profileImage:
+                "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png",
+            }}
           >
-            Registration
-          </Button>
-        </PHForm>
-        <div className="text-center">
-          Already have an account ? <Link href={"/login"}>Login</Link>
+            <div className="py-3">
+              <PHInput label="Name" name="name" size="sm" />
+            </div>
+            <div className="py-3">
+              <PHInput label="Email" name="email" size="sm" />
+            </div>
+            <div className="py-3">
+              <PHInput
+                label="Password"
+                name="password"
+                size="sm"
+                type="password"
+              />
+            </div>
+            <div className="py-3">
+              <PHInput
+                label="Profile Image URL"
+                name="profileImage"
+                size="sm"
+              />
+            </div>
+
+            <Button
+              className="my-3 w-full rounded-md bg-default-900 text-default"
+              size="lg"
+              type="submit"
+            >
+              Registration
+            </Button>
+          </PHForm>
+          <div className="text-center">
+            Already have an account ? <Link href={"/login"}>Login</Link>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
