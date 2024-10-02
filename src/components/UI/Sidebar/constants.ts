e487028b07a50ee9cf623aeb0b1ee33fc66f83a2
Nabiur siddique <nabiursiddique01@gmail.com>
2024-10-02 12:40:58 +0600

    My posts section design is running

--- src/components/UI/Sidebar/constants.ts
+++ src/components/UI/Sidebar/constants.ts
@@ -1,9 +1,11 @@
 export const userLinks = [
   { href: "/profile/createPost", label: "Create Post" },
   { href: "/profile/changePassword", label: "Change Password" },
+  { href: "/profile/myPosts", label: "My Posts" },
 ];
 
 export const adminLinks = [
   { href: "/admin", label: "Admin" },
   { href: "/profile/changePassword", label: "Change Password" },
+  { href: "/profile/myPosts", label: "My Posts" },
 ];
