a1879d51537d5ca351f4a5c5830f4c8d61ce714a
Nabiur siddique <nabiursiddique01@gmail.com>
2024-10-02 20:33:01 +0600

    What do we do section is added in home page

--- src/app/(CommonLayout)/page.tsx
+++ src/app/(CommonLayout)/page.tsx
@@ -1,10 +1,13 @@
 import Landing from "@/src/components/modules/home/Landing";
+import WhatDoWeDo from "@/src/components/modules/home/WhatDoWeDo";
+import { Card } from "@nextui-org/card";
 
 const Home = () => {
   return (
-    <>
+    <Card>
       <Landing />
-    </>
+      <WhatDoWeDo />
+    </Card>
   );
 };
 
